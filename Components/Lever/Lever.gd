4c2c8376e0b5f6aaa65976d122c7c40b52f8161e
j0braun <info@john-braun.de>
2021-09-17 00:14:04 +0200

    discharge on lever, debug output

--- Components/Lever/Lever.gd
+++ Components/Lever/Lever.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+export var energy_cost = 5
+
 var pressed:bool = false
 
 # player interacted with lever
@@ -15,10 +17,13 @@ func interaction_can_interact(interaction_parent: Node):
 	return true
 
 func interaction_interact(interaction):
-	pressed = true
-	$AnimatedSprite.play("default")
-	emit_signal("on_lever_changed")
-	emit_signal("on_lever_pressed")
+	if pressed:
+		return
+	if interaction.get_node(interaction.interaction_parent).discharge_energy(energy_cost):
+		pressed = true
+		$AnimatedSprite.play("default")
+		emit_signal("on_lever_changed")
+		emit_signal("on_lever_pressed")
 
 func interaction_not_interacted(interaction):
 	if pressed:
