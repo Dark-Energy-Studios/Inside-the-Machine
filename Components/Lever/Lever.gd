071ea0d05d6752375ce897c9abafcdf15acf7860
j0braun <info@john-braun.de>
2021-09-16 20:20:49 +0200

    Lever: add animation

--- Components/Lever/Lever.gd
+++ Components/Lever/Lever.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+var pressed:bool = false
+
 # player interacted with lever
 signal on_lever_pressed()
 
@@ -13,9 +15,14 @@ func interaction_can_interact(interaction_parent: Node):
 	return true
 
 func interaction_interact(interaction):
+	pressed = true
+	$AnimatedSprite.play("default")
 	emit_signal("on_lever_changed")
 	emit_signal("on_lever_pressed")
 
 func interaction_not_interacted(interaction):
+	if pressed:
+		$AnimatedSprite.play("default", true)
+		pressed = false
 	emit_signal("on_lever_changed")
 	emit_signal("on_lever_reset")
