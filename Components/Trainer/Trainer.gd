e588c1376526843653cf85f4fd4a7877ac81ba34
Raphael Pour <info@raphaelpour.de>
2021-09-17 23:50:23 +0200

    Trainer: Add sound and stop transition

--- Components/Trainer/Trainer.gd
+++ Components/Trainer/Trainer.gd
@@ -1,7 +1,6 @@
 extends Node
 
-var running:bool = false
-export var recharge_amount = 5
+export var recharge_amount = 2
 
 signal on_trainer_use()
 
@@ -10,19 +9,34 @@ signal on_trainer_reset()
 
 # trainer status has changed
 signal on_trainer_changed()
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 func interaction_can_interact(interaction_parent: Node):
 	return true
 
 func interaction_interact(interaction):
-	if interaction.get_node(interaction.interaction_parent).recharge_energy(recharge_amount):
-		running = true
-		#$AnimatedSprite.play("default")
-		emit_signal("on_trainer_changed")
-		emit_signal("on_trainer_pressed")
-		
-		
+	interaction.get_node(interaction.interaction_parent).recharge_energy(recharge_amount)
 
+	#$AnimatedSprite.play("default")
+	emit_signal("on_trainer_changed")
+	emit_signal("on_trainer_pressed")
+		
+	# Stop timer to prevent stop animation
+	$StopTrainer.stop()
+	
+	# Play fitness animation which just resets the audio properties to the
+	# default
+	$AnimationPlayer.play("fitness")	
+
+	# Start walking sound if loop isn't already running
+	if ! $AudioStreamPlayer.playing:
+		$AudioStreamPlayer.play()
+
+	# Start timer to stop walking sound if the player lefts the trainer
+	$StopTrainer.start()
+
+func _on_StopTrainer_timeout():
+	$AnimationPlayer.play("stop")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "stop":
+		$AudioStreamPlayer.stop()
