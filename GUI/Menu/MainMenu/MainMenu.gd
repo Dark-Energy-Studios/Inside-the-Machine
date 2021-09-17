dbc4fc6c3f39797665826076e1b72f5a6f870740
Raphael Pour <info@raphaelpour.de>
2021-09-17 23:53:57 +0200

    Menu: add intro track

--- GUI/Menu/MainMenu/MainMenu.gd
+++ GUI/Menu/MainMenu/MainMenu.gd
@@ -1,11 +1,16 @@
 extends Control
 
-
-
 func _on_Start_pressed():
-#	get_tree().change_scene("res://Game/Game.tscn")
-	get_tree().change_scene("res://GUI/Intro/GameIntro.tscn")
-
+	var tween = get_node("Tween")
+	tween.interpolate_property($BackgroundAudio, "volume_db",
+			0, -80, 0.1,
+			Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tween.start()
 
 func _on_Exit_pressed():
 	get_tree().quit()
+
+
+func _on_Tween_tween_completed(object, key):
+	print("tween completed")
+	get_tree().change_scene("res://GUI/Intro/GameIntro.tscn")
