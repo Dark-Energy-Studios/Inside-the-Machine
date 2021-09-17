c52a5019720cc37fd25d28cca077ff4de03f72b1
Raphael Pour <info@raphaelpour.de>
2021-09-17 23:53:12 +0200

    Game: add background track
    Game: add egg

---
+++ Game/default.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+var lost_walkman_vibes:bool = false
+
+func interaction_can_interact(interaction_parent: Node):
+	return true
+
+func interaction_interact(interaction):
+	if !lost_walkman_vibes:
+		get_parent().get_node("BackgroundAudio").stop()
+		$AudioStreamPlayer.play()
+	else:
+		$AudioStreamPlayer.stop()
+		get_parent().get_node("BackgroundAudio").play()
+		
+	lost_walkman_vibes = !lost_walkman_vibes
