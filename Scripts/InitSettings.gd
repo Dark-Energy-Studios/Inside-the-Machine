7aa23ff5cc5641256e4336a084bf9ca6e2dd1c8f
Raphael Pour <info@raphaelpour.de>
2021-09-18 10:12:18 +0200

    Settings: add music,fx,master

--- Scripts/InitSettings.gd
+++ Scripts/InitSettings.gd
@@ -6,6 +6,15 @@ func _ready():
 
 	if gets("fullscreen") == null:
 		sets("fullscreen", false)
+	
+	if gets("master_volume") == null:
+		sets("master_volume", -10.0)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), gets("master_volume"))
 
-	if gets("master-volume") == null:
-		sets("master-volume", 100.0)
+	if gets("music_volume") == null:
+		sets("music_volume", 0.0)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), gets("music_volume"))
+	
+	if gets("fx_volume") == null:
+		sets("fx_volume", 0.0)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("FX"), gets("fx_volume"))
