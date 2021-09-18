eefc9360b53c4d7e799737606747b0b69d10c9e9
Raphael Pour <info@raphaelpour.de>
2021-09-18 10:48:32 +0200

    Egg: Fix

--- Game/default.gd
+++ Game/default.gd
@@ -7,10 +7,10 @@ func interaction_can_interact(interaction_parent: Node):
 
 func interaction_interact(interaction):
 	if !lost_walkman_vibes:
-		get_parent().get_node("BackgroundAudio").stop()
+		get_parent().get_node("Game_Music").stop()
 		$Default_Music.play()
 	else:
 		$Default_Music.stop()
-		get_parent().get_node("BackgroundAudio").play()
+		get_parent().get_node("Game_Music").play()
 		
 	lost_walkman_vibes = !lost_walkman_vibes
