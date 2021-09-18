ddee5ec8c30223de74dffa192d2988f65f23de67
j0braun <info@john-braun.de>
2021-09-18 19:54:25 +0200

    update label in own function

--- Components/CountdownClock/CountdownClock.gd
+++ Components/CountdownClock/CountdownClock.gd
@@ -7,25 +7,24 @@ export var duration:int = 60
 var time_remaining:int
 
 func _ready():
-	var minute = duration / 60
-	var seconds = duration % 60
-	var time_label = "%d:%02d" % [minute, seconds]
-	
-	$Sprite/Label.text = time_label
+	update_label(duration)
 	
 
 func _on_Timer_timeout():
 	time_remaining -= 1
-	var minute = time_remaining / 60
-	var seconds = time_remaining % 60
-	var time_label = "%d:%02d" % [minute, seconds]
-	$Sprite/Label.text = time_label
+	update_label(time_remaining)
 	if time_remaining == 0:
 		emit_signal("on_countdown_expired")
 		$Timer.stop()
 	else:
 		emit_signal("on_countdown_tick", time_remaining)	
 
+	
+func update_label(time_remaining):
+	var minute = time_remaining / 60
+	var seconds = time_remaining % 60
+	var time_label = "%d:%02d" % [minute, seconds]
+	$Sprite/Label.text = time_label
 	
 func start():
 	time_remaining = duration
