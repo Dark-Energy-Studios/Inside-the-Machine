ccf471530daabd7cf10fb47a8c8bf5b646a174cd
Raphael Pour <info@raphaelpour.de>
2021-09-19 12:29:11 +0200

    GUI: add win/game over dialog with level switcher

---
+++ GUI/Dialog/Win/Win.gd
@@ -0,0 +1,22 @@
+extends Control
+
+signal on_next()
+
+var level = 0
+
+func _ready():
+	hide()
+	$Firework.one_shot = true
+
+func _on_NextButton_pressed():
+	emit_signal("on_next")
+	
+	var scene = "res://Level/Level{id}/Level{id}.tscn".format({"id" : level})
+	if ResourceLoader.exists(scene):
+		get_tree().change_scene(scene)
+	else:
+		print("Scene '{scene}' doesn't exist!".format({"scene": scene}))
+
+func show_dialog(next_level: int):
+	show()
+	level = next_level
